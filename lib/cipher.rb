e1d6e24498288688799ed4677fd69fadda55b542
Connor Worthington <worthington.connor@gmail.com>
2020-09-29 19:18:20 +0100

    Adds conditional to .scramble

--- lib/cipher.rb
+++ lib/cipher.rb
@@ -1,10 +1,11 @@
 class Cipher 
 
   def scramble(string, shift)
-    # a = (string.ord + (shift - 97)) % 26 + 97
-    # a.chr
-    a = (string.ord + (shift - 65)) % 26 + 65
-    a.chr
+    if lowercase?(string)
+      ((string.ord + (shift - 97)) % 26 + 97).chr
+    else
+      ((string.ord + (shift - 65)) % 26 + 65).chr
+    end 
   end 
 
   def uppercase?(char)
